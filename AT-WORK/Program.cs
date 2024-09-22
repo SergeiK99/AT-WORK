7af976eb0cee5aad78ecd97a209b6df100643848
SergeiK99 <skx2013@mail.ru>
2024-09-22 20:31:02 +0700

    Initial commit

---
+++ AT-WORK/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AT_WORK
+{
+    internal class Program
+    {
+        public static bool IsPalindrome(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                throw new ArgumentException("Введенная строка пустая!");
+
+            text = text.ToLower().Replace(" ", "");
+            return text == new string(text.Reverse().ToArray());
+        }
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(text) ? "Палиндромом" : "Не палиндромом");
+        }
+    }
+}
